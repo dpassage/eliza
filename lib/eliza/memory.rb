870dc6270e2e986a8f249948a8b17cd2f2c53af5
David Paschich <dpassage@balveda.com>
2014-05-13 08:59:08 -0700

    Fix more style bugs

--- lib/eliza/memory.rb
+++ lib/eliza/memory.rb
@@ -1,20 +1,18 @@
 module Eliza
   class Memory < Array
-    
     def initialize
       super
       @max = 20
     end
-    
+
     def get
       shift
     end
-    
+
     def save(s)
       push s
       shift if length > @max
       nil
     end
-    
   end
 end
--- lib/eliza/script.rb
+++ lib/eliza/script.rb
@@ -30,66 +30,68 @@ module Eliza
             expr = @syns[$1] or raise "error: unknown synonym: '#{$1}'."
             decomp.pattern = $` + "(#{expr})" + $'
           end
-          #puts "final pattern(#{decomp.pattern})"
+          # puts "final pattern(#{decomp.pattern})"
           decomp.pattern = /#{decomp.pattern}/i
         end
       end
     end
-    
+
     def pre_translate(str)
       translate(@pre, str)
     end
-    
+
     def post_translate(str)
       translate(@post, str)
     end
-    
+
     private
-    
+
     def parse(inputScript)
-      
       key, decomp = nil
-      
-      File.new(inputScript).each { |line|
-        
+
+      File.new(inputScript).each do |line|
+
         case line
-          
+
         when /^\s*#/                 then # comment line
         when /^\s*final:(.*)/        then @final = $1.strip
         when /^\s*initial:(.*)/      then @initial = $1.strip
         when /^\s*quit:(.*)/         then @quits.push $1.strip
-          
+
         when /^\s*pre:\s*(\S+)(.*)/
           src, dst = $1.downcase, $2.strip
           @pre[src] = PrePost.new(src, dst)
-          
+
         when /^\s*post:\s*(\S+)(.*)/
           src, dst = $1.downcase, $2.strip
           @post[src] = PrePost.new(src, dst)
-          
+
         when /^\s*synon:(.*)/
           names = $1.downcase.split.sort
           pattern = names.join('|')
           names.each { |name| @syns[name] = pattern }
-          
+
         when /^\s*key:(.*)/
           name, rank = $1.downcase.split
           key = @keys[name] = Key.new(name, rank, @keys.size - 1)
           decomp = nil
-          
+
         when /^\s*decomp:\s*(.*)/
           key or raise "#@scriptName(#$.): Error: no key for decomp."
           pattern = $1.downcase.gsub('**', '*').gsub('*', ' * ').strip.squeeze(' ')
-          #puts "=== before: #{pattern} ==="
+          # puts "=== before: #{pattern} ==="
           words = pattern.split
-          if words[0] == "$" then mem = true; words.shift
-          else mem = false end
+          if words[0] == "$"
+            mem = true; words.shift
+          else
+            mem = false
+          end
           raise "#{@scriptName}($.): error: empty decomp!" if words.size == 0
           pattern = ''
-          words.each_index { |i|
+          words.each_index do |i|
             if words[i] == '*'
               if i == words.size - 1  # '*' at end of sentence (greedy)
-                pattern += '(.*)' 
+                pattern += '(.*)'
               elsif i > 0             # '*' between words
                 pattern += '((?:\s(?:\S+\s)*?)+?)'
               else                    # '*' at beginning
@@ -99,27 +101,26 @@ module Eliza
               pattern += words[i]
               pattern += ' ' if i < words.size - 1 and words[i+1] != '*'
             end
-          }
-          #puts "=== after: #{pattern} ==="
-          key.push( decomp = Decomp.new( pattern, mem ))
-          
+          end
+          # puts "=== after: #{pattern} ==="
+          key.push(decomp = Decomp.new(pattern, mem))
+
         when /^\s*reasmb:(.*)/
           decomp or raise "#@scriptName($.): Error: no decomp for reasmb!"
           decomp.push $1.strip
-          
+
         else
           $stderr << "Unrecognized script input, line #$.: #{line}\n"
         end
-      }
-    end
-    
-    def translate(map, str)
-     (words = str.split).each_index { |i|
-        pre_post = map[words[i].downcase]
-        words[i] = pre_post.dst if pre_post
-      }
-      words.join ' '
+      end
     end
 
+    def translate(map, str)
+      (words = str.split).each_index do |i|
+        pre_post = map[words[i].downcase]
+        words[i] = pre_post.dst if pre_post
+      end
+      words.join ' '
+    end
   end
 end
